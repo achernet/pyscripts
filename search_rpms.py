ceb798151b86cbc2d122fd53b9498b441806eb50
Alex Chernetz <alex.chernetz@concur.com>
2016-03-07 22:20:38 -0800

    Allow for multiple retries when searching for RPM packages

--- search_rpms.py
+++ search_rpms.py
@@ -1,14 +1,41 @@
 #!/usr/bin/env python
-import requests
-from lxml.html import etree, HTMLParser
-import re
-import urlparse
-import os
-from collections import Iterable
-import sys
 import argparse
+from collections import Iterable
+import contextlib
+import fnmatch
+import logging
+import os
+import re
+import requests
+import socket
+import sys
+import urllib2
+import urlparse
+from lxml.html import etree, HTMLParser
 from sh import aria2c
 import retrying
+from path import Path
+
+if not logging.root.handlers:
+    logging.basicConfig(format='%(asctime)s [%(levelname)s]: %(message)s')
+logger = logging.getLogger('search_rpms')
+logger.setLevel(logging.DEBUG)
+
+
+class TimeoutContext(object):
+
+    def __init__(self, timeout):
+        self.new_timeout = timeout
+        self.old_timeout = socket.getdefaulttimeout()
+
+    def __enter__(self):
+        logger.info('Setting socket timeout to %.3fs...', self.new_timeout)
+        socket.setdefaulttimeout(self.new_timeout)
+        return self
+
+    def __exit__(self, *exc_info):
+        socket.setdefaulttimeout(self.old_timeout)
+        return False
 
 
 class SearchWrapper(object):
@@ -38,8 +65,9 @@ class SearchWrapper(object):
                      'simple': 1,
                      'srodzaj': 4,
                      'limit': page}
-        resp = self.session.post(url, data=post_data, cookies=cookie_dict,
-                                 timeout=(5, 21))
+        with TimeoutContext(5):
+            resp = self.session.post(url, data=post_data, cookies=cookie_dict,
+                                     timeout=(5, 21))
         tree = etree.fromstring(resp.content, HTMLParser())
         tree.make_links_absolute(resp.url)
         return tree
@@ -53,7 +81,10 @@ class SearchWrapper(object):
         else:
             count = rgx.search(etree.tostring(tree))
         if not count:
+            logger.error('Failed to count links in search page, returning 0')
             return 0
+        links = int(count.group(1))
+        logger.info('Number of links found in search page: %d', links)
         return int(count.group(1))
 
     @staticmethod
@@ -74,6 +105,12 @@ class SearchWrapper(object):
             if os.path.splitext(fname)[-1] == '.rpm':
                 rpm_html = '{0}.html'.format(fname)
                 rpm_dict.pop(rpm_html, None)
+        # show any HTML entries left over as full links
+        for fname, flinks in rpm_dict.items():
+            if os.path.splitext(fname)[-1] == '.html':
+                rpm_dict.pop(fname)
+                for flink in flinks:
+                    rpm_dict[flink] = set([flink])
         return rpm_dict
 
 
@@ -107,11 +144,56 @@ def name_key(*args):
     return major_parts
 
 
+def _find_valid_file(mirror_file, mirror_data):
+    valid_file = None
+    for i in xrange(0, mirror_file.count('-') + 1):
+        search_term = '{0}-*'.format('-'.join(mirror_file.split('-')[:i + 1]))
+        if mirror_file in search_term:
+            search_term = mirror_file
+        search_pat = fnmatch.translate(search_term)
+        search_pat = '(?m){0}'.format(search_pat.replace('\\Z(?ms)', ''))
+        rgx = re.compile(search_pat)
+        matches = rgx.findall(mirror_data)
+        if len(matches) == 1:
+            valid_file = matches[0].rstrip()
+            break
+    return valid_file
+
+
+def find_valid_mirrors(mirrors):
+    valid_file = None
+    for mirror in mirrors:
+        mpr = urlparse.urlparse(mirror)._asdict()
+        mirror_file = Path(mpr['path']).basename()
+        mpr['path'] = Path(mpr['path']).dirname()
+        parent_url = urlparse.ParseResult(**mpr).geturl()
+        try:
+            with TimeoutContext(5):
+                with contextlib.closing(urllib2.urlopen(parent_url)) as f:
+                    data = f.read()
+        except Exception as e:
+            logger.exception('Error opening mirror site: %s', e.__class__.__name__)
+            data = ''
+        valid_file = _find_valid_file(mirror_file, data)
+        if valid_file is not None:
+            break
+    if valid_file is None:
+        logger.error('Error validating mirrors: No valid file was found\n%s', mirrors)
+        return []
+    for i, m in enumerate(mirrors):
+        mpr = urlparse.urlparse(m)._asdict()
+        mpr['path'] = Path(mpr['path']).dirname().joinpath(valid_file)
+        mirrors[i] = urlparse.ParseResult(**mpr).geturl()
+    return mirrors
+
+
 def do_download(mirrors):
     mirrors = sorted(mirrors) if not isinstance(mirrors, list) else mirrors
-    name = mirrors[0]
-    print('Downloading {0}...'.format(name))
-    for line in aria2c(mirrors, _iter_noblock=True):
+    logger.info('List of mirrors: %s', mirrors)
+    valid_mirrors = find_valid_mirrors(mirrors)
+    name = valid_mirrors[0]
+    logger.info('Downloading %s...', name)
+    for line in aria2c(valid_mirrors, _iter_noblock=True):
         if isinstance(line, basestring):
             sys.stdout.write(line)
             sys.stdout.flush()
@@ -126,16 +208,16 @@ def main(args=None):
     )
     ap.add_argument('query', help='The query to search for')
     ns = ap.parse_args(args)
-    print('Searching for {0}...'.format(ns.query))
+    logger.info('Searching for %s...', ns.query)
     rpm_dict = {}
     with SearchWrapper(ns.query) as sw:
         page = sw.search_rpm_page()
         count = sw.parse_count(page)
         num_pages = (count / 100) + bool(count % 100)
-        print('Found {0} matches'.format(count))
+        logger.info('Found %d matches', count)
         rpm_dict = sw.parse_rpm_links(page, rpm_dict)
         for index in xrange(2, num_pages + 1):
-            print('Getting page {0} of results...'.format(index))
+            logger.info('Getting page %d/%d of results...', index, num_pages)
             page = sw.search_rpm_page(index)
             rpm_dict = sw.parse_rpm_links(page, rpm_dict)
     if len(rpm_dict) == 1:
